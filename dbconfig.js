f7172b5bc49b37cdece271ebec730d1bea434c95
Albin B <Baawa@users.noreply.github.com>
2018-03-10 13:46:05 +0100

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -3,15 +3,11 @@ module.exports = {
     accessKey           : 'oZCq3CqRuK24Bt2txchdOuRHyw36eJ6hf1G1QENi'
   },
   db: {
-    database            : process.env.RDS_DB_NAME  || 'Random',
+    database            : process.env.RDS_DB_NAME  || 'dat076_db',
     host                : process.env.RDS_HOSTNAME || 'localhost',
     port                : process.env.RDS_PORT     || 3306,
     user                : process.env.RDS_USERNAME || 'root',
-<<<<<<< HEAD
-    password            : process.env.RDS_PASSWORD || 'mysql',
-=======
-    password            : process.env.RDS_PASSWORD || 'djshaotime',
->>>>>>> origin/master
+    password            : process.env.RDS_PASSWORD || 'root',
     multipleStatements  : true
   }
 };
