df06a2847ec4c0b85073302b3b6da57bd9418fe3
Pedram Shirmohammad <pedram.shir@hotmail.com>
2018-03-10 13:28:31 +0100

    User page and update on header
    ser page and update on header

--- dbconfig.js
+++ dbconfig.js
@@ -7,7 +7,7 @@ module.exports = {
     host                : process.env.RDS_HOSTNAME || 'localhost',
     port                : process.env.RDS_PORT     || 3306,
     user                : process.env.RDS_USERNAME || 'root',
-    password            : process.env.RDS_PASSWORD || 'root',
+    password            : process.env.RDS_PASSWORD || 'mysql',
     multipleStatements  : true
   }
 };
