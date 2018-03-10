4d535503810b0020c042a3247b9f71202159d146
Eric Shao <shaotime@Erics-MacBook-Pro.local>
2018-03-10 18:28:00 +0100

    restored albins cookies

--- dbconfig.js
+++ dbconfig.js
@@ -7,11 +7,11 @@ module.exports = {
     host                : process.env.RDS_HOSTNAME || 'localhost',
     port                : process.env.RDS_PORT     || 3306,
     user                : process.env.RDS_USERNAME || 'root',
-<<<<<<< HEAD
+
     password            : process.env.RDS_PASSWORD || 'djshaotime',
-=======
+
     password            : process.env.RDS_PASSWORD || 'mysql',
->>>>>>> f3f6d445bf7c1dd42bd95a972d336f53a04eba96
+
     multipleStatements  : true
   }
 };
