24b2c36ae6eb618bb4c2c3e546a7f273b7349805
Albin B <Baawa@users.noreply.github.com>
2018-03-12 00:09:05 +0100

    Fixed merge conflict
    A last minute push resulted in a merge conflict.

--- dbconfig.js
+++ dbconfig.js
@@ -3,18 +3,11 @@ module.exports = {
     accessKey           : 'oZCq3CqRuK24Bt2txchdOuRHyw36eJ6hf1G1QENi'
   },
   db: {
-    database            : process.env.RDS_DB_NAME  || 'Random',
+    database            : process.env.RDS_DB_NAME  || 'dat076_db',
     host                : process.env.RDS_HOSTNAME || 'localhost',
     port                : process.env.RDS_PORT     || 3306,
     user                : process.env.RDS_USERNAME || 'root',
-<<<<<<< HEAD
-
-    password            : process.env.RDS_PASSWORD || 'djshaotime',
-
-
-=======
     password            : process.env.RDS_PASSWORD || 'root',
->>>>>>> 002ee8ec1f3ff444885af0fe5b4fe8e0c647bc74
     multipleStatements  : true
   }
 };
